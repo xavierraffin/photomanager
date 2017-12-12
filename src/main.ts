425cfacc26d9b70da6f7f3a79a0dfc725f1cd4bb
Xavier Raffin <xavier.raffin@gmail.com>
2017-12-11 20:48:55 -0800

    Send stored settings to mainwindow

--- src/main.ts
+++ src/main.ts
@@ -2,21 +2,42 @@ const path = require('path');
 const url = require('url');
 
 import {app, BrowserWindow, Menu, ipcMain} from 'electron';
+import {Store} from './utils/Store';
+import { Logger, LOG_LEVEL } from "./utils/Logger";
 
 // SET ENV
 //process.env.NODE_ENV = 'production';
 process.env.NODE_ENV = 'development';
+var logger: Logger = new Logger(LOG_LEVEL.DEBUG);
+
+const store = new Store(
+  {
+    // 800x600 is the default size of our window
+    'windowBounds': { 'width': 1024, 'height': 600 },
+    'storageDir': '/nodefs'
+  }
+);
 
 let mainWindow: BrowserWindow;
 
 app.on('ready', function(){
 
-  mainWindow = new BrowserWindow({});
+  let { width, height  } = store.get('windowBounds');
+  logger.log(LOG_LEVEL.DEBUG, "Create window of %sx%s", width, height);
+  mainWindow = new BrowserWindow({width, height, 'show': false});
+
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, '../resources/view/mainWindow.html'),
     protocol: 'file:',
     slashes: true
   }));
+
+
+  mainWindow.once('ready-to-show', function(){
+    mainWindow.webContents.send('folder:set', store.get('storageDir'));
+    logger.log(LOG_LEVEL.INFO, store.get('storageDir'));
+    mainWindow.show();
+  });
 
   // Quit on cross
   mainWindow.on('closed', function(){
