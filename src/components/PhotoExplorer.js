a2762f66f042f8466b88cc14b70314877c453359
Xavier Raffin <xavier.raffin@gmail.com>
2017-12-15 19:18:37 -0800

    Test layout with 100 images

--- src/components/PhotoExplorer.js
+++ src/components/PhotoExplorer.js
@@ -3,42 +3,24 @@ import { Grid, Row, Col } from 'react-bootstrap';
 import './PhotoExplorer.css';
 
 class PhotoExplorer extends Component {
+  createRow() {
+    return (
+      <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
+        <div className="photoCell">
+        </div>
+      </Col>
+    )
+  }
+
   render() {
+    var cells = [];
+    for (var i = 0; i < 100; i++) {
+      cells[i] = this.createRow();
+    }
     return (
       <Grid bsClass="photoGrid">
         <Row bsClass="reset">
-          <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
-            <div className="photoCell">
-            </div>
-          </Col>
-          <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
-            <div className="photoCell">
-            </div>
-          </Col>
-          <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
-            <div className="photoCell">
-            </div>
-          </Col>
-          <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
-            <div className="photoCell">
-            </div>
-          </Col>
-          <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
-            <div className="photoCell">
-            </div>
-          </Col>
-          <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
-            <div className="photoCell">
-            </div>
-          </Col>
-          <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
-            <div className="photoCell">
-            </div>
-          </Col>
-          <Col className="photoBox" xs={12} sm={6} md={3} lg={2}>
-            <div className="photoCell">
-            </div>
-          </Col>
+          {cells}
         </Row>
       </Grid>
     );
